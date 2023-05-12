3248b74a6231a6c1e922bac441d5a7b72584da32
Joao <joaopvieira@ua.pt>
2023-05-12 17:26:22 +0100

    Patient Menu Update

--- rtd_project/lib/patient/main_patient.dart
+++ rtd_project/lib/patient/main_patient.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:rtd_project/main.dart';
+
+//
 
 void main() {
   runApp(MainPatient());
-}//MANDAR FORA NO FIM!
+}// ------------------------------------------------------> MANDAR FORA NO FIM!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 class MainPatient extends StatefulWidget {
   @override
@@ -16,13 +17,15 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
     return MaterialApp( // ----------------------------------------------------> P1: Scaffold tem de ser Child de MaterialApp
         debugShowCheckedModeBanner: false,
         title: 'Rede Transporte de Doentes',
-        theme: ThemeData( primarySwatch: Colors.blue,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
           appBarTheme: AppBarTheme(
             iconTheme: IconThemeData(color: Colors.black),
-            color: Colors.redAccent,
+            color: Colors.redAccent
           ),
-        ),
+          ),
         home: Scaffold(
+          backgroundColor: Colors.black,
           resizeToAvoidBottomInset : false, // --------------------------------> P2: Evita Overflow de Pixels **
           appBar: AppBar(
             title: Text("Paciente - Menu Principal")
@@ -34,6 +37,8 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
                   shrinkWrap: true,
                   primary: true,
                   crossAxisCount: 2,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
                   children: <Widget>[
 // #############################################################################
 // ################ SCHEDULE - UPPER LEFT BUTTON ###############################
@@ -41,15 +46,15 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
                     ElevatedButton.icon(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey, // ---------------------> Button Color (Background color)
-                        foregroundColor: Colors.white, // --------------------> Text Color  (Foreground color)
+                        backgroundColor: Colors.white, // ---------------------> Button Color (Background color)
+                        foregroundColor: Colors.blueAccent, // --------------------> Text Color  (Foreground color)
                       ),
                       icon: const Icon(
-                        color: Colors.white, // ------------------------------> Icon Color
+                        color: Colors.blueAccent, // ------------------------------> Icon Color
                         Icons.local_hospital,
                         size: 80,
                       ),
-                      label: const Text('Agendamentos', style: TextStyle(fontSize: 25)), // ---> Text & its size
+                      label: const Text('Agendamentos', style: TextStyle(fontSize: 25), textAlign: TextAlign.center, ), // ---> Text & its size
                     ),
 // #############################################################################
 // ################ PATIENT - UPPER RIGHT BUTTON ###############################
@@ -57,12 +62,12 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
                     ElevatedButton.icon(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey, // ---------------------> Button Color (Background color)
-                        foregroundColor: Colors.white, // --------------------> Text Color  (Foreground color)
+                        backgroundColor: Colors.white, // ---------------------> Button Color (Background color)
+                        foregroundColor: Colors.blueAccent, // --------------------> Text Color  (Foreground color)
                       ),
                       icon: const Icon(
-                        color: Colors.white, // ------------------------------> Icon Color
-                        Icons.person,
+                        color: Colors.blueAccent, // ------------------------------> Icon Color
+                        Icons.account_box,
                         size: 80,
                       ),
                       label: const Text('Utentes', style: TextStyle(fontSize: 25)), // ---> Text & its size
@@ -73,11 +78,11 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
                     ElevatedButton.icon(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey, // ---------------------> Button Color (Background color)
-                        foregroundColor: Colors.white, // --------------------> Text Color  (Foreground color)
+                        backgroundColor: Colors.white, // ---------------------> Button Color (Background color)
+                        foregroundColor: Colors.blueAccent, // --------------------> Text Color  (Foreground color)
                       ),
                       icon: const Icon(
-                        color: Colors.white, // ------------------------------> Icon Color
+                        color: Colors.blueAccent, // ------------------------------> Icon Color
                         Icons.phone,
                         size: 80,
                       ),
@@ -89,23 +94,17 @@ class _PatientState extends State<MainPatient> with SingleTickerProviderStateMix
                     ElevatedButton.icon(
                       onPressed: () {},
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.grey, // ---------------------> Button Color (Background color)
-                        foregroundColor: Colors.white, // --------------------> Text Color  (Foreground color)
+                        backgroundColor: Colors.white, // ---------------------> Button Color (Background color)
+                        foregroundColor: Colors.blueAccent, // --------------------> Text Color  (Foreground color)
                       ),
                       icon: const Icon(
-                        color: Colors.white, // ------------------------------> Icon Color
+                        color: Colors.blueAccent, // ------------------------------> Icon Color
                         Icons.notifications,
                         size: 80,
                       ),
                       label: const Text('Notificações', style: TextStyle(fontSize: 25)), // ---> Text & its size
                     ),
                   ],
-                ),
-// #############################################################################
-// ################ BUTTON DIVIDER #############################################
-// #############################################################################
-                const Divider(
-                  color: Colors.black,
                 ),
 // #############################################################################
 // ################ ENDING #####################################################
