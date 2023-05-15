fb527fbbff8d0ceeeeb827fa713d9192e6cfc558
LaraMatos <lara.catarina.matos@ua.pt>
2023-05-15 12:54:47 +0100

    confirmation page done

---
+++ rtd_project/lib/patient/confirmation_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class ConfirmationPage extends StatefulWidget {
+  const ConfirmationPage({Key? key}) : super(key: key);
+
+  @override
+  _ConfirmationPageState createState() => _ConfirmationPageState();
+}
+
+class _ConfirmationPageState extends State<ConfirmationPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        /*
+        actions: const [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 5, 10, 5),
+            child: Image(image: AssetImage("assets/images/LogotipoBVCE_branco.png"),
+            ),
+          ),
+        ],
+         */
+        leading: const BackButton(
+          color: Colors.white,
+        ),
+        title: const Text("Página de Confirmação"),
+        backgroundColor: Color.fromARGB(230, 152, 0, 1),
+      ),
+
+      body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+
+               Column(
+                 children: [
+
+                      Padding(
+                        padding: const EdgeInsets.all(5),
+                        child: Text('Aguarde um momento...',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            color: Color.fromARGB(230, 152, 0, 1),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text('O seu pedido está a ser processado. \nSerá informado quando for aprovado',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            //color: Colors.teal,
+                            //fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            child: Text('OK'),
+                        ),
+                      ),
+
+
+                    ],
+                  ),
+
+
+
+              ],
+            ),
+
+          )
+
+      ),
+
+    );
+  }
+}
